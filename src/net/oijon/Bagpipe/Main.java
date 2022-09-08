a5fb88bc8a71da725a05493e7313f86d9269749f
N3ther <thenetherreactor@gmail.com>
2022-09-08 14:36:58 -0400

    made main runnable

--- src/net/oijon/Bagpipe/Main.java
+++ src/net/oijon/Bagpipe/Main.java
@@ -3,6 +3,7 @@ package net.oijon.Bagpipe;
 public class Main {
 
 	public static void main(String[] args) {
-		Tube tube = new Tube();
+		Tube tube = new Tube(9, 0.5F, 2);
+		System.out.println("Frequencies: " + tube.getFrequencies(36000).toString());
 	}
 }
