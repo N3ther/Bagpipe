2093c58ab01a66b5b8ca886ecdb7ffad51ae224b
N3ther <thenetherreactor@gmail.com>
2022-09-08 23:52:07 -0400

    Main now prints frequencies of tube.

--- src/net/oijon/Bagpipe/Main.java
+++ src/net/oijon/Bagpipe/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Tube tube = new Tube(9, 0.5F, 2);
-		System.out.println("Frequencies: " + tube.getFrequencies(36000).toString());
+		
+		System.out.print("Frequencies: ");
+		
+		float[] frequencies = tube.getFrequencies(32000);
+		
+		for (int i = 0; i < frequencies.length - 1; i++) {
+			System.out.print(frequencies[i] + ", ");
+		}
+		System.out.print(frequencies[frequencies.length - 1]);
+		System.out.print("\n");
 	}
 }
