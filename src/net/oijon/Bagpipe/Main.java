6bae15e4adc72c329f7bceb16b43f06fd780f879
N3ther <alex@consus.fun>
2022-10-08 21:57:58 -0400

    fixed logic involving frequency generation

--- src/net/oijon/Bagpipe/Main.java
+++ src/net/oijon/Bagpipe/Main.java
@@ -3,16 +3,39 @@ package net.oijon.Bagpipe;
 public class Main {
 
 	public static void main(String[] args) {
-		Tube tube = new Tube(9, 0.5F, 2);
+		Tube tube = new Tube(12, 3, 1);
+		
+		Tube helmholtzTube = new Tube(8, 4, 1);
 		
 		System.out.print("Frequencies: ");
 		
-		float[] frequencies = tube.getFrequencies(32000);
+		float[] frequencies = tube.getFrequencies();
 		
-		for (int i = 0; i < frequencies.length - 1; i++) {
-			System.out.print(frequencies[i] + ", ");
+		/** Sorts frequencies
+		for (int i = 0; i < frequencies.length; i++) {
+			if (i != frequencies.length - 1) {
+				if (frequencies[i] > frequencies[i + 1]) {
+					float tempFloat = frequencies[i + 1];
+					frequencies[i + 1] = frequencies[i];
+					frequencies[i] = tempFloat;
+					i = 0;
+				}
+			}
+			
+			if (i != 0) {
+				if (frequencies[i] < frequencies[i - 1]) {
+					float tempFloat = frequencies[i - 1];
+					frequencies[i - 1] = frequencies[i];
+					frequencies[i] = tempFloat;
+					i = 0;
+				}
+			}
 		}
-		System.out.print(frequencies[frequencies.length - 1]);
+		**/
+		for (int i = 0; i < frequencies.length; i++) {
+			System.out.print(frequencies[i] + ",\n");
+		}
+		
 		System.out.print("\n");
 	}
 }
--- src/net/oijon/Bagpipe/Tube.java
+++ src/net/oijon/Bagpipe/Tube.java
@@ -7,7 +7,9 @@ public class Tube {
 	private float area; // cm^2
 	private int amountOfOpenings; // 0 to 2
 	
+	private int velocity = 34300; // speed of sound, cm/s
 	
+	//Creates the tube
 	public Tube(float length, float radius, int amountOfOpenings) {
 		this.length = length;
 		this.radius = radius;
@@ -16,8 +18,10 @@ public class Tube {
 				+ (float) (2 * Math.PI * (radius * radius));
 	}
 	
-	public float[] getFrequencies(float velocity) {
-		float[] frequencies = new float[3];
+	//Generates frequencies produced based off tube properties
+	public float[] getFrequencies() {
+		float[] frequencies = new float[5]; //max of 5 frequencies modeled at once
+		
 		
 		if (amountOfOpenings < 0) {
 			System.err.println("Tube attempted to be created with " +
@@ -30,24 +34,41 @@ public class Tube {
 			amountOfOpenings = 2;
 		}
 		
-		switch (amountOfOpenings) {
-		case 0:
-			frequencies[0] = velocity / (2 * length);
-			frequencies[1] = velocity / length;
-			frequencies[2] = (3 * velocity) / (2 * length);
-			break;
-		case 1:
-			frequencies[0] = velocity / (4 * length);
-			frequencies[1] = (3 * velocity) / (4 * length);
-			frequencies[2] = (5 * velocity) / (4 * length);
-			break;
-		case 2:
-			frequencies[0] = velocity / (2 * length);
-			frequencies[1] = velocity / length;
-			frequencies[2] = (3 * velocity) / (2 * length);
-			break;
+		// f = c/2L
+		if (amountOfOpenings % 2 == 0) {
+			
+			for (int i = 0; i < 5; i++) {
+				frequencies[i] = (i * 2) * (velocity / (2 * length));
+			}
+			
+		// f = c/4L
+		} else {
+			
+			for (int i = 0; i < 5; i++) {
+				frequencies[i] = (i * 2 + 1) * (velocity / (4 * length));
+			}
 		}
 		return frequencies;
 	}
 	
+	public float getLength() {
+		return length;
+	}
+	
+	public float getRadius() {
+		return radius;
+	}
+	
+	public float getArea() {
+		return area;
+	}
+	
+	public int getOpenings() {
+		return amountOfOpenings;
+	}
+	
+	public float getHelmholtz(Tube resonator, Tube opening) {
+		float frequency = (float) ((velocity / (2 * Math.PI)) * Math.sqrt((opening.getRadius() * 2) / resonator.getArea() * opening.getLength()));
+		return frequency;
+	}
 }
